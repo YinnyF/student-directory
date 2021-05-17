dd425a4f3c183ceef58393f6fc3cab8f0598f81a
Cynthia Fu <yinnycfu@gmail.com>
2021-05-17 11:05:07 +0000

    updated students to an array of student hashes

--- directory.rb
+++ directory.rb
@@ -1,16 +1,16 @@
-# put all students into an array
+# put all students into an array of hashes
 students = [
-  "Dr. Hannibal Lecter",
-  "Darth Vader",
-  "Nurse Ratched",
-  "Michael Corleone",
-  "Alex DeLarge",
-  "The Wicked Witch of the West",
-  "Terminator",
-  "Freddy Kreuger",
-  "The Joker",
-  "Joffrey Baratheon",
-  "Norman Bates"
+  {name: "Dr. Hannibal Lecter", cohort: :november},
+  {name: "Darth Vader", cohort:  :november},
+  {name: "Nurse Ratched", cohort:  :november},
+  {name: "Michael Corleone", cohort:  :november},
+  {name: "Alex DeLarge", cohort:  :november},
+  {name: "The Wicked Witch of the West", cohort:  :november},
+  {name: "Terminator", cohort:  :november},
+  {name: "Freddy Kreuger", cohort:  :november},
+  {name: "The Joker", cohort:  :november},
+  {name: "Joffrey Baratheon", cohort:  :november},
+  {name: "Norman Bates", cohort:  :november}
 ]
 
 def print_header
@@ -18,9 +18,9 @@ def print_header
   puts "-------------"
 end
 
-def print(names) # names is an array
-  names.each do |name|
-    puts name
+def print(students) # students is an array of hashes
+  students.each do |student| # student is a hash
+    puts "#{student[:name]} (#{student[:cohort]} cohort)"
   end
 end
 
